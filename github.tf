8cde30481db4fa6d6758476070e4853cf442a342
Brayan Marin Guirales <93493999+Merxxotas@users.noreply.github.com>
2024-06-25 09:57:35 -0500

    Delete github.tf

--- github.tf
+++
@@ -1,7 +0,0 @@
-#Create a GitHub Repository
-resource "github_repository" "repo_since_terraform" {
-  name        = "first_repo_since_terraform"
-  description = "My awesome skills with terraform"
-
-  visibility = "public"
-}
